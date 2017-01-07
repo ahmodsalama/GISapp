a60b45e3ccc85f73a51045e8de047190fa44b7d9
Ahmad Salama <Ahmad Salama@AHMAD-SALAMA>
2017-01-07 12:09:59 -0800

    adding new project

---
+++ GISapp/GISapp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GISapp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GISapp/GISapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GISapp.Global" Language="C#" %>
